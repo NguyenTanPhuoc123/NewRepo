3604634248df046c1cb1ec4ba094e9c1e205101d
Tấn Phước Nguyễn <ngguyentanphuoc2003@gmail.com>
2023-03-18 18:07:11 +0700

    edit frmManager

--- frmLogin/FrmManager.Designer.cs
+++ frmLogin/FrmManager.Designer.cs
@@ -41,12 +41,23 @@ namespace frmLogin
             this.panelTitleBar = new System.Windows.Forms.Panel();
             this.buttonExit = new System.Windows.Forms.Button();
             this.btnMinimumized = new System.Windows.Forms.Button();
+            this.btnMaximumized = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
             this.panelMain = new System.Windows.Forms.Panel();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.btnMaximumized = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelTitleBar.SuspendLayout();
+            this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -195,6 +206,20 @@ namespace frmLogin
             this.btnMinimumized.UseVisualStyleBackColor = true;
             this.btnMinimumized.Click += new System.EventHandler(this.btnMinimumized_Click);
             // 
+            // btnMaximumized
+            // 
+            this.btnMaximumized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximumized.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMaximumized.BackgroundImage")));
+            this.btnMaximumized.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMaximumized.FlatAppearance.BorderSize = 0;
+            this.btnMaximumized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximumized.Location = new System.Drawing.Point(652, 5);
+            this.btnMaximumized.Name = "btnMaximumized";
+            this.btnMaximumized.Size = new System.Drawing.Size(41, 32);
+            this.btnMaximumized.TabIndex = 1;
+            this.btnMaximumized.UseVisualStyleBackColor = true;
+            this.btnMaximumized.Click += new System.EventHandler(this.btnMaximumized_Click);
+            // 
             // lblTitle
             // 
             this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -209,6 +234,18 @@ namespace frmLogin
             // 
             // panelMain
             // 
+            this.panelMain.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelMain.BackgroundImage")));
+            this.panelMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelMain.Controls.Add(this.label10);
+            this.panelMain.Controls.Add(this.label9);
+            this.panelMain.Controls.Add(this.label8);
+            this.panelMain.Controls.Add(this.label7);
+            this.panelMain.Controls.Add(this.label6);
+            this.panelMain.Controls.Add(this.label5);
+            this.panelMain.Controls.Add(this.label4);
+            this.panelMain.Controls.Add(this.label3);
+            this.panelMain.Controls.Add(this.label2);
+            this.panelMain.Controls.Add(this.label1);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(220, 97);
             this.panelMain.Name = "panelMain";
@@ -220,19 +257,117 @@ namespace frmLogin
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
-            // btnMaximumized
+            // label1
             // 
-            this.btnMaximumized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMaximumized.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMaximumized.BackgroundImage")));
-            this.btnMaximumized.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnMaximumized.FlatAppearance.BorderSize = 0;
-            this.btnMaximumized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMaximumized.Location = new System.Drawing.Point(652, 5);
-            this.btnMaximumized.Name = "btnMaximumized";
-            this.btnMaximumized.Size = new System.Drawing.Size(41, 32);
-            this.btnMaximumized.TabIndex = 1;
-            this.btnMaximumized.UseVisualStyleBackColor = true;
-            this.btnMaximumized.Click += new System.EventHandler(this.btnMaximumized_Click);
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(129, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CỬA HÀNG ĂN VẶT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(299, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PAV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "THÀNH";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "KÍNH";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "PHÂN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "THÂN";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(651, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "TIÊU";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(648, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "DIÊU";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(655, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "CỰC";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(632, 248);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 32);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "NHỌC";
             // 
             // FrmManager
             // 
@@ -253,6 +388,8 @@ namespace frmLogin
             this.panelMenu.ResumeLayout(false);
             this.panelTitleBar.ResumeLayout(false);
             this.panelTitleBar.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -273,5 +410,15 @@ namespace frmLogin
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Button btnMaximumized;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
